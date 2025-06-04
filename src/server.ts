f88afa49c9f1a6cdc21910e8adf86b232f09534f
Juan Veronez <juan.monteirov@gmail.com>
2025-06-04 14:28:58 -0300

    build: add ESLint, extends an existing config and add a script to run it

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
 app.get('/hello', (req, res) => {
-  res.send({ message: 'Hello, NodeJS!' });
-});
+  res.send({ message: 'Hello, NodeJS!' })
+})
 
 app.listen({ port: 3333 }).then(() => console.log('HTTP Server Running'))
