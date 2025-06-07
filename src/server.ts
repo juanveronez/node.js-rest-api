1d55bdb45e415c7bedb0c86b8dca7bc35f113c5e
Juan Veronez <juan.monteirov@gmail.com>
2025-06-07 18:25:58 -0300

    feat: create some routes to test simple knex queries

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,17 @@ import { knex } from './infra/database'
 
 const app = fastify()
 
-app.get('/hello', async (req, res) => {
-  const data = await knex('sqlite_schema').select('*')
-  console.log(data)
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({ title: 'Teste', amount: 1000 })
+    .returning('*')
 
-  res.send({ message: 'Hello, NodeJS!' })
+  return transaction
+})
+
+app.get('/world', async () => {
+  const transactions = await knex('transactions').select('*')
+  return transactions
 })
 
 app.listen({ port: 3333 }).then(() => console.log('HTTP Server Running'))
