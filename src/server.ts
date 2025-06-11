5552884c41103e75e2dae3c3432bc1d97b2e7512
Juan Veronez <juan.monteirov@gmail.com>
2025-06-11 18:49:18 -0300

    feat: finish project

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,8 @@ import { app } from './app'
 import { env } from './env'
 
 app
-  .listen({ host: '0.0.0.0', port: env.PORT })
+  .listen({
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+    port: env.PORT,
+  })
   .then(() => console.log('HTTP Server Running'))
